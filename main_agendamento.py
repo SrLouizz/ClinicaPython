18fdf2aae53c88b466c4c1f1740d768652325e61
yguinho <luizfelipesantiagodasilva4@gmail.com>
2024-06-06 10:14:14 -0300

    agendamento

--- main_agendamento.py
+++ main_agendamento.py
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
@@ -7,44 +6,37 @@ agendas = []
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', agendas=agendas)
 
-@app.route('/agendamento')
+@app.route('/agendamento', methods=['GET', 'POST'])
 def agendamento():
     if request.method == 'POST':
-        codigo = len(agenda)
+        codigo = len(agendas)
         nome_do_animal = request.form['nome_do_animal']
         nome_do_tutor = request.form['nome_do_tutor']
         data = request.form['data']
         hora = request.form['hora']
         sintomas = request.form['sintomas']
-        agendas.append(codigo, nome_animal, nome_tutor, data, hora, sintomas)
+        agendas.append((codigo, nome_do_animal, nome_do_tutor, data, hora, sintomas))
         return redirect('/')
-    else:
-        return render_template('agendamento.html')
-        
-@app.route('/agendamento/<int:codigo>', methods=['GET', 'POST'])
-def editar_consulta(codigo):
+    return render_template('agendamento.html')
 
+@app.route('/editar_agendamento/<int:codigo>', methods=['GET', 'POST'])
+def editar_consulta(codigo):
     if request.method == 'POST':
         data = request.form['data']
         hora = request.form['hora']
         sintomas = request.form['sintomas']
-        return redirect('/') 
+        agendas[codigo] = (codigo, agendas[codigo][1], agendas[codigo][2], data, hora, sintomas)
+        return redirect('/')
     else:
         agenda = agendas[codigo]
         return render_template('agendamento.html', agenda=agenda)
 
-@app.route('/agendamento/<int:codigo>')
+@app.route('/cancelar_agendamento/<int:codigo>')
 def cancelar_consulta(codigo):
-
-    del login[codigo]
+    del agendas[codigo]
     return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
-=======
-from flask import Flask
-
-app = __Flask__
->>>>>>> Stashed changes
